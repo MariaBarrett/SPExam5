b511ae999006b08354c6d5b2634773ec10e3d6f0
Alexander Wahl-Rasmussen <a.wahl.r@gmail.com>
2013-12-03 10:46:01 +0100

    Some small changes for temporary raw_input

--- openfile.py
+++ openfile.py
@@ -7,11 +7,23 @@ f2 = open("data/tagged","r+") #read and write
 print f2.read()
 
 for line in f1:
+	print "-" * 50
+	print "Please annotate the following tweet token by token"
 	print line
+
 	"Guangliangs code goes here"
 	f2.write(line)
 
+	for word in line:
+		print word
+		tag = raw_input("Enter the POS-tag: ")
+		if tag not in allowed:
+			"reject!"
+
+
 print f2.read()
+
+allowed = ['ADV','VERB','NOUN','ADP','PRON','DET','.','PRT','NUM','X','CONJ','ADJ']
 
 f1.close() #Remember to close whenever you're done with the file!
 f2.close() #Same same

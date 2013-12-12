4e2eaf2dabbc29966bf03c5b7680189f19de8045
MariaBarrett <Maria@d009.edur.sc.ku.dk>
2013-12-12 14:04:43 +0100

    Openfile now contains a working tagging interface. Though I'm unsure how to split so that punctuation is split into separate tokens
    Many special characters are allowed in hyperlinks and we don’t want to
    split a hyperlink into more tokens. We don’t want to split by @ and #
    either, but that’s simple

--- SP_Assignment5.py
+++ SP_Assignment5.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 
-#tweets = ('tweets.txt')
-tweets = ('/Users/Maria/Documents/ITandcognition/Github/SPExam5/tweets.txt')
-#test_file = ('/Users/Maria/Documents/ITandcognition/bin/twitter-POS/test.google')
+from __future__ import division
+import sys, glob
+import collections
+from collections import Counter
+import numpy as np
+
+
+tweets = ('tweets.txt')
 
 #split by double newline aka by every new tweet
 tweets2 = open(tweets).read().split("\n\n")
 
 tweetlist = [i.split() for i in tweets2]
 
+
+
+
+POS_list=['adv', 'noun', 'adp', 'prt', 'det', 'num', '.', 'pron', 'verb', 'x', 'conj', 'adj']
+
 for tweet in tweetlist:
-	print tweet
-	for word in tweet:
-		print word
+    print tweet  
+    for word in tweet:
+        pos=raw_input('please input pos for %s:\n' % word)
+
+
+        while( pos not in POS_list):
+            print 'Please input the POS that excists in adv, noun, adp, prt, det, num, ., pron, verb, x, conj, adj, thanks '
+            pos=raw_input('please input pos for %s:\n' % word)
+
 
 """
 def prepare_data(train_list):
--- openfile.py
+++ openfile.py
@@ -1,29 +1,41 @@
+from __future__ import division
+import sys, glob
+import collections
+from collections import Counter
+import numpy
 
 #### Opening and closing data files! ####
 
-f1 = open("data/untagged","r") #only read
-f2 = open("data/tagged","r+") #read and write
+#f1 = open("data/untagged","r") #only read
+#f2 = open("data/tagged","r+") #read and write
 
-print f2.read()
+f1 = open("/Users/Maria/Documents/ITandcognition/Github/SPExam5/data/untagged","r") #only read
+f2 = open("/Users/Maria/Documents/ITandcognition/Github/SPExam5/data/tagged","r+") #read and write
 
-for line in f1:
+#split by double newline aka by every new tweet
+tweets2 = f1.read().split("\n\n")
+
+tweetlist = [i.split() for i in tweets2]
+
+
+allowed = ['ADV','VERB','NOUN','ADP','PRON','DET','.','PRT','NUM','X','CONJ','ADJ']
+#print f2.read()
+
+for line in tweetlist:
 	print "-" * 50
 	print "Please annotate the following tweet token by token"
 	print line
 
-	"Guangliangs code goes here"
-	f2.write(line)
-
 	for word in line:
-		print word
-		tag = raw_input("Enter the POS-tag: ")
-		if tag not in allowed:
-			"reject!"
-
-
-print f2.read()
-
-allowed = ['ADV','VERB','NOUN','ADP','PRON','DET','.','PRT','NUM','X','CONJ','ADJ']
+		tag = raw_input('please input pos for: %s ' % word)
+		while tag not in allowed:
+			tag = raw_input('please input pos for %s ' % word)
+		else:
+			f2.write(word)
+			f2.write('\t')
+			f2.write(tag)
+		f2.write('\n')
+	f2.write('\n\n')
 
 f1.close() #Remember to close whenever you're done with the file!
 f2.close() #Same same
